16a3ea1a8d7835a6ef5236b060df63c107e6c057
Ait Mansour Mohamed <uapv1802814@ecv-2w26-105.univ-avignon.fr>
2018-03-16 18:06:40 +0100

    Correct minor error

--- Q6-JAVA ClientTextCase Example/ServiceConsumer.java
+++ Q6-JAVA ClientTextCase Example/ServiceConsumer.java
@@ -5,10 +5,13 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import javax.xml.ws.WebServiceRef;
 import com.dataaccess.webservicesserver.*;
+	
 public class ServiceConsumer {
-	@WebServiceRef(wsdlLocation="https://www.dataaccess.com/webservicesserver/textcasing.wso?WSDL")
+		
+		@WebServiceRef(wsdlLocation="http://www.dataaccess.com/webservicesserver/textcasing.wso?WSDL")
+		static TextCasing service ;
+
     public static void main(String[] args) throws Exception {
-		TextCasing service = new TextCasing();
 		TextCasingSoapType soapService = service.getTextCasingSoap();
          System.out.println(soapService.invertStringCase("Mohamed"));
 
